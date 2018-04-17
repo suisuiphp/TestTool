61338a57423880db5f8c21eb45facb4cf373e1fe
zhuoyan <18108347985@163.com>
2018-04-17 23:21:01 +0800

    add decode resolution

--- Common/FileServer/FileServer.py
+++ Common/FileServer/FileServer.py
@@ -1,26 +1,26 @@
 # encoding:utf-8
-import chardet
+import chardet,uniout
 class FileServer(object):
     def __init__(self):
         pass
 
     @staticmethod
-    def __file_open(file, format):
+    def __file_open(filename, format):
         try:
-            file = open(file, format)
+            file = open(filename, format)
         except Exception as e:
             print("*ERROR* ", repr(e))
         return file
 
     @staticmethod
-    def __file_close(filename):
-        if filename != None:
+    def __file_close(file):
+        if file != None:
             try:
                 file.close()
             except Exception as e:
                 print("*ERROR* ", repr(e))
 
-    def list_lines_data(self, file, format):
+    def file_readlines(self, filename, format):
         '''
         按行读取TXT文件中所以数据
         :param file:
@@ -28,8 +28,7 @@ class FileServer(object):
         :return: 返回行数数据列表
         '''
         # 打开文件
-        print("start---")
-        file = self.__file_open(file, format)
+        file = self.__file_open(filename, format)
         # 读文件到列表
         list_data = []
         if file != None:
@@ -38,8 +37,25 @@ class FileServer(object):
         self.__file_close(file)
         return list_data
 
-    def fileWrite(self, file, format, str):
-        file = self.__file_open(file, format)
+    def file_read(self,filename,format):
+        '''
+        读取TXT文件中所有数据
+        :param file:
+        :param format:
+        :return: str
+        '''
+        # 打开文件
+        file = self.__file_open(filename, format)
+        # 读文件到列表
+        str_data = []
+        if file != None:
+            str_data = file.read()
+        # 关闭文件
+        self.__file_close(file)
+        return str_data
+
+    def fileWrite(self, filename, format, str):
+        file = self.__file_open(filename, format)
         if file != None:
             try:
                 file.write(str)
@@ -50,13 +66,25 @@ class FileServer(object):
 
 
 if __name__ == "__main__":
-
+    file = "/Users/yan/PycharmProjects/TestTool/department_tags.txt"
+    open_format = "rb"
     fileserver = FileServer()
-    list_data = fileserver.list_lines_data("C:\zhuoyan_work\zhuoyan\PyCharm\DBCompare\test.txt","rb")
-    print list_data
-    # file = fileserver.open("test.txt", "rb")
-    # data = file.read()
+
+    str_data = fileserver.file_read(file,open_format)
+    # print str_data.decode("gb2312")
+    list_data = str_data.split("\n")
+    print(list_data)
+
+
+
+
+    #验证文件编码格式
+    # f = open(file, "rb")
+    # data = f.read()
     # print(chardet.detect(data))
+    # f.close()
+
+
     # print(data.decode("utf-8"))
     # try:
     #     file = open("tes.txt","rb")
--- DepartmentTags/DepartmentTags.py
+++ DepartmentTags/DepartmentTags.py
@@ -1,32 +1,46 @@
 #encoding:utf-8
+import chardet
+import uniout
 from Common.FileServer.FileServer import FileServer
 class DepartmentTags(object):
     def __init__(self):
         self.file_server = FileServer()
 
-    def list_file_data(self,file,format):
-        list_data = self.file_server.list_lines_data(file,format)
+    def get_filedata(self,file,format):
+        str_data = self.file_server.file_read(file,format)
+        return str_data
+
+    def list_filedata(self,file,format):
+        list_data = self.file_server.file_readlines(file,format)
         return list_data
 
-    def decode_listdata(self,list,format):
-        return [element.decode(format) for element in list]
+    def decode_data(self,data,code_format):
+        if isinstance(data,str):
+            return data.decode(code_format)
+        elif isinstance(data,list):
+            return [element.decode(code_format) for element in data]
+        else:
+            print("*ERROR* decode data type can only be str,list!")
 
     def get_refrence_tags(self,file,open_format,code_format):
         #获取TXT文件数据到列表中
-        tags_data = self.list_file_data(file,open_format)
+        tags_data = self.list_filedata(file,open_format)
+        print("原始文件数据列表：",tags_data)
         #将列表数据按需要的格式进行解码
-        tags_data = self.decode_listdata(tags_data,code_format)
+        tags_data = self.decode_data(tags_data,code_format)
         #建立科室-标签字典
         dept_tags = {}
         for dept_tag in tags_data:
-            dept_tag = str(dept_tag)
-            dept_tag = dept_tag.replace("\n","")
+            dept_tag = dept_tag.replace("\n","") #去除多余换行符
             dept_tag = dept_tag.replace("\r","")
             dept_tag = dept_tag.replace(" ","")
             dept_tags[dept_tag.split(":")[0]] = dept_tag.split(":")[1]
         return dept_tags
 
 if __name__=="__main__":
+    file = "/Users/yan/PycharmProjects/TestTool/department_tags.txt"
+    open_format = "rb"
+    code_format = "gb2312"
     deptServer = DepartmentTags()
-    dept_tags = deptServer.get_refrence_tags("C:\zhuoyan_work\zhuoyan\PyCharm\DBCompare\test.txt","rb","utf-8")
+    dept_tags = deptServer.get_refrence_tags(file,open_format,code_format)
     print dept_tags
