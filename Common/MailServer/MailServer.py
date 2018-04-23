ee19025489aaa29716026619301de34e91328cbf
zhuoyan <18108347985@163.com>
2018-04-23 17:25:42 +0800

    邮邮件附件功能

--- Common/MailServer/MailServer.py
+++ Common/MailServer/MailServer.py
@@ -1,7 +1,9 @@
 #encoding:utf-8
 import smtplib
-from email.mime.text import MIMEText
 from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 
 class MailServer(object):
 
@@ -16,29 +18,35 @@ class MailServer(object):
         self.mail_user = user
         self.mail_pass = passwd
 
-    def send_mail(self,receivers,title,content):
+    def send_mail(self,receivers,title,content,attachment=None):
         '''
         发送邮件
         :param receivers: ["1106045430@qq.com","18108347985@163.com"]收件人
         :param title: 邮件标题 str
         :param content: 邮件正文 str
+        :param attachment: 附件路径"C:/GIT/TestTool/DepartmentTags/result.xls"
         :return:
         '''
-        sender = "18108347985@163.com"  #发件人
-        msg = MIMEText(content, "plain", "utf-8")  #中文需参数utf-8，单自己字符不需要
-        msg["Subject"] = Header(title,"utf-8")
-        msg["From"] = sender
+        msg =  MIMEMultipart()
+        msg["Subject"] = title
+        msg["From"] = self.mail_user
         msg["To"] = ";".join(receivers)
-        # message["Subject"] = Header("missyou","utf-8")
 
+        part = MIMEText(content,"plain", "utf-8")
+        msg.attach(part)
+
+        if attachment != None:
+            part = MIMEApplication(open(attachment,"rb").read())
+            part.add_header('Content-Disposition', 'attachment', filename=attachment)
+            msg.attach(part)
         try:
             s = smtplib.SMTP()  #创建邮件服务器对象
             s.connect(self.mail_host, 25) #连接到指定的smtp服务器，参数分别表示smpt主机和端口
             s.login(self.mail_user, self.mail_pass)
-            s.sendmail(sender, receivers, msg.as_string())
-            print "邮件发送成功"
+            s.sendmail(self.mail_user, receivers, msg.as_string())
+            print "*INFO* 邮件发送成功"
         except Exception as e:
-            print "Error:无法发送邮件"
+            print "*ERROR* 无法发送邮件"
             print repr(e)
         finally:
             s.close()
@@ -48,6 +56,7 @@ if __name__ == "__main__":
     receiver = ["1106045430@qq.com","18108347985@163.com"]
     title = "放假通知"
     content = "python mail test"
-    mail_server.send_mail(receiver,title,content)
+    attachment = "C:/GIT/TestTool/DepartmentTags/result.xls"
+    mail_server.send_mail(receiver,title,content,attachment)
 
 
